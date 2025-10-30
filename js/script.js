5f014887ce083e35408a2c41c8fb590e677ec070
Baleshevich Srdjan <37658865+BaleshSrle@users.noreply.github.com>
2025-10-30 16:51:05 +0100

    Refactor input and output fields, improve JS selectors
    Removed redundant 'form-control' classes from input and output elements in index.html and corrected some aria-labels. In script.js, improved jQuery selector usage with arrow functions, fixed typos in attribute names, and enhanced table responsiveness and styling by wrapping tables and adjusting captions. Also corrected input attribute assignments and added missing 'form-control' class to output elements via JS.

--- js/script.js
+++ js/script.js
@@ -1,42 +1,42 @@
-$(document).ready(function () {
+$(document).ready(() => {
     "use strict";
     //console.info("Glavna skripta je pokrenuta.");
     const images = document.images;
     const scripts = document.scripts;
-    $("head").each(function () {
-        $(this).prepend($("<meta>").attr({ "name": "mobile-web-app-capable", "content": "yes" }), $("<meta>").attr({ "name": "apple-mobile-web-app-capable", "content": "yes" }), $("<meta>").attr({ "name": "MSThemeCompatible", "content": "yes" }), $("<meta>").attr({ "http-equiv": "cleartype", "content": "on" }), $("<meta>").attr({ "http-equiv": "x-dns-prefetch-control", "content": "on" }), $("<meta>").attr({ "http-equiv": "Cache-control", "content": "no-cache" }), $("<meta>").attr({ "http-equiv": "Pragma", "content": "no-cache" }), $("<meta>").attr({ "http-equiv": "Expires", "content": "-1" }));
-        $(this).prepend($("<meta>").attr({ "name": "author", "content": "Baleševi&#263; Sr&#273;an, srdjan.b%40teol.net" }), $("<meta>").attr({ "name": "reply-to", "content": "srdjan.balesevic%40yahoo.com" }), $("<meta>").attr({ "name": "language", "content": "sr" }), $("<meta>").attr({ "name": "host", "content": "pages.github.com" }), $("<meta>").attr({ "name": "geo.region", "content": "BA-SRP" }), $("<meta>").attr({ "name": "geo.placename", "content": "Добој" }), $("<meta>").attr({ "name": "twitter:creator", "content": "@BaleshSrle" }), $("<meta>").attr({ "name": "twitter:site", "content": "@BaleshSrle" }));
-        $(this).append($("<link>").attr({ "rel": "stylesheet", "href": "https://cdn.jsdelivr.net/npm/flag-icons/css/flag-icons.min.css", "crossorigin": "anomymous" }).on("error", function () { this.href = "https://cdnjs.cloudflare.com/ajax/libs/flag-icons/7.5.0/css/flag-icons.min.css" }), $("<link>").attr({ "rel": "dns-prefetch", "href": "https://cdn.jsdelivr.net/" }), $("<link>").attr({ "rel": "preconnect", "href": "https://cdn.jsdelivr.net/", "crossorigin": "anonymous" }), $("<link>").attr({ "rel": "prefetch", "href": "https://img.shields.io/" }));
-        $(this).children("script:lt(2)").attr("crossorigin", "anonymous");
+    $("head").each((_, e) => {
+        $(e).prepend($("<meta>").attr({ "name": "mobile-web-app-capable", "content": "yes" }), $("<meta>").attr({ "name": "apple-mobile-web-app-capable", "content": "yes" }), $("<meta>").attr({ "name": "MSThemeCompatible", "content": "yes" }), $("<meta>").attr({ "http-equiv": "cleartype", "content": "on" }), $("<meta>").attr({ "http-equiv": "x-dns-prefetch-control", "content": "on" }), $("<meta>").attr({ "http-equiv": "Cache-control", "content": "no-cache" }), $("<meta>").attr({ "http-equiv": "Pragma", "content": "no-cache" }), $("<meta>").attr({ "http-equiv": "Expires", "content": "-1" }));
+        $(e).prepend($("<meta>").attr({ "name": "author", "content": "Baleševi&#263; Sr&#273;an, srdjan.b%40teol.net" }), $("<meta>").attr({ "name": "reply-to", "content": "srdjan.balesevic%40yahoo.com" }), $("<meta>").attr({ "name": "language", "content": "sr" }), $("<meta>").attr({ "name": "host", "content": "pages.github.com" }), $("<meta>").attr({ "name": "geo.region", "content": "BA-SRP" }), $("<meta>").attr({ "name": "geo.placename", "content": "Добој" }), $("<meta>").attr({ "name": "twitter:creator", "content": "@BaleshSrle" }), $("<meta>").attr({ "name": "twitter:site", "content": "@BaleshSrle" }));
+        $(e).append($("<link>").attr({ "rel": "stylesheet", "href": "https://cdn.jsdelivr.net/npm/flag-icons/css/flag-icons.min.css", "crossorigin": "anonymous" }).on("error", function () { this.href = "https://cdnjs.cloudflare.com/ajax/libs/flag-icons/7.5.0/css/flag-icons.min.css" }), $("<link>").attr({ "rel": "dns-prefetch", "href": "https://cdn.jsdelivr.net/" }), $("<link>").attr({ "rel": "preconnect", "href": "https://cdn.jsdelivr.net/", "crossorigin": "anonymous" }), $("<link>").attr({ "rel": "prefetch", "href": "https://img.shields.io/" }));
+        $(e).children("script:lt(2)").attr("crossorigin", "anonymous");
     });
-    $("meta:last").after($("<link>").attr({ "rel": "stylesheet", "href": "https://cdn.jsdelivr.net/npm/bootstrap@4/dist/css/bootstrap.min.css", "crossorigin": "anomymous" }).on("error", function () { this.href = "https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.2/css/bootstrap.min.css" }));
+    $("meta:last").after($("<link>").attr({ "rel": "stylesheet", "href": "https://cdn.jsdelivr.net/npm/bootstrap@4/dist/css/bootstrap.min.css", "crossorigin": "anonymous" }).on("error", function () { this.href = "https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.2/css/bootstrap.min.css" }));
 
     //$("body").css("background", "url('https://upload.wikimedia.org/wikipedia/sr/6/68/Saobra%C4%87ajni_fakultet_Doboj.png') right bottom / 200px no-repeat fixed padding-box padding-box");
     $("body").css("background", "url('https://sf.ues.rs.ba/cir/wp-content/uploads/2024/04/LogoPng.png') right bottom / 200px no-repeat fixed padding-box padding-box");
     $("div.container-fluid").addClass("p-3")
     $("h2").each(function () {
         $(this).parent("div.card-header").addClass("py-2");
-        $(this).children("button").each(function (index) {
-            $("h2").children("button").attr({ "type": "button", "data-toggle": "collapse", "aria-controls": $("h2 > button").eq(index).attr("data-target").replace("#", "") })
+        $(this).children("button").each(function (i) {
+            $("h2").children("button").attr({ "type": "button", "data-toggle": "collapse", "aria-controls": $("h2 > button").eq(i).attr("data-target").replace("#", "") })
         }).addClass("btn btn-link btn-block text-left");
     }).addClass("mb-0");
-    $("div.collapse").each(function (index) {
-        $(this).attr({ "id": $("h2 > button").eq(index).attr("data-target").replace("#", ""), "aria-labelledby": $(this).prev().attr("id"), "data-parent": "#" + $("div.accordion").attr("id") });
+    $("div.collapse").each(function (i) {
+        $(this).attr({ "id": $("h2 > button").eq(i).attr("data-target").replace("#", ""), "aria-labelledby": $(this).prev().attr("id"), "data-parent": "#" + $("div.accordion").attr("id") });
     });
     $("div#collapseTransportProduce > div.card-body div.col-sm-6:last").append($("<p></p>").attr("id", "rezultat"), $("<p></p>").attr("id", "rezultat2"), $("<p></p>").attr("id", "rezultat3"), $("<p></p>").attr("id", "rezultat4"), $("<p></p>").attr("id", "rezultat5"), $("<p></p>").attr("id", "rezultat6"));
     $("div#collapseADR > div.card-body").append($("<h5></h5>").addClass("pt-3").text("Značenje identifikacionog broja opasnosti"), $("<p></p>").addClass("card-text"), $("<p></p>").addClass("text-muted mb-0").html("&#42;&nbsp;Voda se smije koristiti samo uz odobrenje stručnjaka.<br>Neke opasne materije burno reaguju u kontaktu sa vodom &#40;od ispuštanja otrovnih gasova do samozapaljenja&#41;, zato je potrebno pažljivo rukovanje sa vodom."));
-    $("div.card-body:eq(0)").each(function () {
-        $(this).children("h5").addClass("card-title");
-        $(this).children("p").addClass("card-text");
-        $(this).find("var").addClass("font-weight-bold px-1");
-        $(this).find("span:not(.fi)").addClass("font-italic");
+    $("div.card-body:eq(0)").each((_, e) => {
+        $(e).children("h5").addClass("card-title");
+        $(e).children("p").addClass("card-text");
+        $(e).find("var").addClass("font-weight-bold px-1");
+        $(e).find("span:not(.fi)").addClass("font-italic");
     });
     $("div.alert").attr("role", "alert");
     $("div.alert:eq(0)").addClass("alert-primary").append($("<h4></h4>").addClass("alert-heading").text("Napomena"), $("<p></p>").addClass("mb-0").html("Ova kolekcija aplikacija je namjenjena za sve korisnike iz<span class='fi fi-rs align-middle mx-1' title='Srbija'></span>,<span class='fi fi-me align-middle mx-1' title='Crna Gora'></span>,<span class='fi fi-ba align-middle mx-1' title='Bosna i Hercegovina'></span>i<span class='fi fi-hr align-middle mx-1' title='Hrvatska'></span>."));
     $("div.alert:eq(1)").addClass("alert-info").append($("<h4></h4>").addClass("alert-heading").text("Info"), $("<p></p>").addClass("mb-0").html("Podaci za ovu web aplikaciju su iskorišteni iz skripte <strong>Transport opasnih materija - ADR 2007</strong> čiji su autori Prof. dr Velibor Peulić, dr Radovan Višković, docent i Željko Matoc"));
     $("article").not(":first").prepend($("<hr>").addClass("my-2"));
     $("figure:eq(0)").addClass("figure text-center").append($("<img>").attr({ "src": "https://api.inpart24.com/uploads/yoofp01b.nze_adr-plate.jpg", "alt": "ADR Listica" }).addClass("figure-img img-fluid my-n3").css("max-height", "200px"), $("<figcaption></figcaption>").addClass("figure-caption text-center font-italic").text("Primjer naranžastog obilježja sa brojem za obilježavanje opasnosti (HIN) i UN brojem")).parent("div").addClass("d-flex justify-content-center");
-    $("div.card-footer").addClass("text-muted py-1").append($("<p></p>").addClass("mb-0 small text-center").html("Ovaj projekat koristi<img alt='Bootstrap Badge'>,<img alt='jQuery Badge'>,<img alt='Chart.js Badge'>preko<img alt='jsDelivr Badge'>CDN servera i hostovan je na<img alt='GitHub Pages Badge'>.<br>HTML kod ove stranice možete vidjeti na<a href='https://github.com/BaleshSrle/SFDobojWebAppCollection' hreflang='en' target='_blank'><img alt='Static Badge'></a>.<br>&copy; 2019 - " + new Date().getFullYear() + "."));
+    $("div.card-footer").addClass("text-muted py-1").append($("<p></p>").addClass("mb-0 small text-center").html("Ovaj projekat koristi<img alt='Bootstrap Badge'>,<img alt='jQuery Badge'>,<img alt='Chart.js Badge'>preko<img alt='jsDelivr Badge'>CDN servera i hostovan je na<img alt='GitHub Pages Badge'>.<br>HTML kod ove stranice možete vidjeti na<a href='https://github.com/BaleshSrle"+location.pathname+"' hreflang='en' target='_blank'><img alt='Static Badge'></a>.<br>&copy; 2019 - " + new Date().getFullYear() + "."));
     images[1].src = "https://img.shields.io/github/v/release/twbs/bootstrap?filter=v4.*&logo=bootstrap&logoColor=white&label=Bootstrap&color=7952b3";
     images[1].onerror = function () { this.src = "https://badgen.net/jsdelivr/v/npm/bootstrap@4?color=7952b3&label=Bootstrap"; }
     images[2].src = "https://img.shields.io/github/v/release/jquery/jquery?filter=3.*&logo=jquery&logoColor=white&label=jQuery&color=0769ad";
@@ -56,17 +56,20 @@ $(document).ready(function () {
     }).addClass("mb-3"); */
     /* $("div.input-group-prepend > span").addClass("input-group-text"); */
     $("select, input").addClass("form-control").attr("required", true);
+    $("output").addClass("form-control");
     $("select > option:first-child").attr({ "selected": true, "disabled": true });
-    $("input[type='number']").each(function () {
+    $("input[type='number']").each((_, e) => {
         //$(this).not(":lt(7):gt(3)").attr("inputmode", "numeric");
         //$(this).slice(4, 7).attr("inputmode", "decimal");
-        $(this).filter("#OutsideTemp, #ProduceWeigh, #ProduceTemp, #TransportDuration, #P1x, #P1y, #P2x, #P2y, #P3x, #P3y, #Sx, #Sy").attr("inputmode", "numeric");
-        $(this).filter("#Q1, #Q2, #Q3").attr("inputmode", "decimal");
-        $(this).filter("#ProduceWeigh, #TransportDuration").attr({ "min": "1", "skip": "1" }).parent("div.form-group").tooltip({ placement: 'top', title: 'U ovo polje se unosi pozitivni cijeli broj.' });
-        $(this).filter("#OutsideTemp, #ProduceTemp").attr({ "min": "-50", "max": "50", "step": "1", "pattern": "-?[0-9]{2}" }).parent("div.form-group").tooltip({ placement: 'top', title: 'U ovo polje se unosi pozitivni ili negativni cijeli broj.' });
+        $(e).filter("#OutsideTemp, #ProduceWeigh, #ProduceTemp, #TransportDuration, #P1x, #P1y, #P2x, #P2y, #P3x, #P3y, #Sx, #Sy").attr("inputmode", "numeric");
+        $(e).filter("#Q1, #Q2, #Q3").attr("inputmode", "decimal");
+        $(e).filter("#ProduceWeight, #TransportDuration").attr({ "min": "1", "step": "1" }).parent("div.form-group").tooltip({ placement: 'top', title: 'U ovo polje se unosi pozitivni cijeli broj.' });
+        $(e).filter("#OutsideTemp, #ProduceTemp").attr({ "min": "-50", "max": "50", "step": "1", "pattern": "-?[0-9]{2}" }).parent("div.form-group").tooltip({ placement: 'top', title: 'U ovo polje se unosi pozitivni ili negativni cijeli broj.' });
     });
-    $("table.table").addClass("table-sm text-center").wrap($("<div></div>").addClass("table-responsive"));
-    $("table.table > caption").addClass("text-center");
+    $("table.table").each((_, e) => {
+        $(e).addClass("table-sm text-center").wrap($("<div></div>").addClass("table-responsive"));
+        $(e).children("caption").addClass("text-center").css("caption-side", "top");
+    });
     $("td").prev("th").addClass("align-middle");
     $("button.btn-primary").on("click", function () {
         var a = $("#RefrigerantType").val();
