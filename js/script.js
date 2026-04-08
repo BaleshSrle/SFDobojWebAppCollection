2850d8fdf67da5fbdb4dcade81f64e8b354841b3
Baleshevich Srdjan <srdjan.balesevic@yahoo.com>
2026-04-08 22:05:12 +0200

    Refactoring HTML & JS Code

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 $(document).ready(() => {
     "use strict";
-    //console.info("Glavna skripta je pokrenuta.");
+    // console.info("Glavna skripta je pokrenuta.");
+    const UrlCDN = ["https://cdn.jsdelivr.net/", "https://cdnjs.cloudflare.com/"];
+    const BadgesUrl = ["https://img.shields.io/", "https://badgen.net/"];
+    const GitHubUrl = ["pages.github.com", "https://github.com/", "https://gist.githubusercontent.com/"];
     const images = document.images;
-    const scripts = document.scripts;
+    // const scripts = document.scripts;
     $("head").each((_, e) => {
         $(e).prepend($("<meta>").attr({ "name": "mobile-web-app-capable", "content": "yes" }), $("<meta>").attr({ "name": "apple-mobile-web-app-capable", "content": "yes" }), $("<meta>").attr({ "name": "MSThemeCompatible", "content": "yes" }), $("<meta>").attr({ "http-equiv": "cleartype", "content": "on" }), $("<meta>").attr({ "http-equiv": "x-dns-prefetch-control", "content": "on" }), $("<meta>").attr({ "http-equiv": "Cache-control", "content": "no-cache" }), $("<meta>").attr({ "http-equiv": "Pragma", "content": "no-cache" }), $("<meta>").attr({ "http-equiv": "Expires", "content": "-1" }));
-        $(e).prepend($("<meta>").attr({ "name": "author", "content": "Baleševi&#263; Sr&#273;an, srdjan.b%40teol.net" }), $("<meta>").attr({ "name": "reply-to", "content": "srdjan.balesevic%40yahoo.com" }), $("<meta>").attr({ "name": "language", "content": "sr" }), $("<meta>").attr({ "name": "host", "content": "pages.github.com" }), $("<meta>").attr({ "name": "geo.region", "content": "BA-SRP" }), $("<meta>").attr({ "name": "geo.placename", "content": "Добој" }), $("<meta>").attr({ "name": "twitter:creator", "content": "@BaleshSrle" }), $("<meta>").attr({ "name": "twitter:site", "content": "@BaleshSrle" }));
-        $(e).append($("<link>").attr({ "rel": "stylesheet", "href": "https://cdn.jsdelivr.net/npm/flag-icons/css/flag-icons.min.css", "crossorigin": "anonymous" }).on("error", function () { this.href = "https://cdnjs.cloudflare.com/ajax/libs/flag-icons/7.5.0/css/flag-icons.min.css" }), $("<link>").attr({ "rel": "dns-prefetch", "href": "https://cdn.jsdelivr.net/" }), $("<link>").attr({ "rel": "preconnect", "href": "https://cdn.jsdelivr.net/", "crossorigin": "anonymous" }), $("<link>").attr({ "rel": "prefetch", "href": "https://img.shields.io/" }));
-        $(e).children("script:lt(2)").attr("crossorigin", "anonymous");
+        $(e).prepend($("<meta>").attr({ "name": "author", "content": "Baleševi&#263; Sr&#273;an, srdjan.b%40teol.net" }), $("<meta>").attr({ "name": "reply-to", "content": "srdjan.balesevic%40yahoo.com" }), $("<meta>").attr({ "name": "language", "content": "sr" }), $("<meta>").attr({ "name": "host", "content": GitHubUrl[0] }), $("<meta>").attr({ "name": "geo.region", "content": "BA-SRP" }), $("<meta>").attr({ "name": "geo.placename", "content": "Добој" }), $("<meta>").attr({ "name": "twitter:creator", "content": "@BaleshSrle" }), $("<meta>").attr({ "name": "twitter:site", "content": "@BaleshSrle" }));
+        $(e).append($("<link>").attr({ "rel": "stylesheet", "href": UrlCDN[0] + "npm/flag-icons/css/flag-icons.min.css", "crossorigin": "anonymous" }).on("error", function () { this.href = UrlCDN[1] + "ajax/libs/flag-icons/7.5.0/css/flag-icons.min.css" }), $("<link>").attr({ "rel": "dns-prefetch", "href": UrlCDN[0] }), $("<link>").attr({ "rel": "preconnect", "href": UrlCDN[0], "crossorigin": "anonymous" }), $("<link>").attr({ "rel": "prefetch", "href": BadgesUrl[0] }).on("error", function () { this.href = BadgesUrl[1] }));
+        //$(e).children("script:lt(2)").attr("crossorigin", "anonymous");
+        $(e).children("script[src*='jquery@3']").attr("crossorigin", "anonymous").after($.getScript(UrlCDN[0] + "npm/jquery-migrate@4/dist/jquery-migrate.min.js"), $.getScript(UrlCDN[0] + "npm/bootstrap@4/dist/js/bootstrap.bundle.min.js").fail(() => { $.getScript(UrlCDN[1] + "ajax/libs/bootstrap/4.6.2/js/bootstrap.bundle.min.js"); }), $.getScript(UrlCDN[0] + "npm/chart.js@2/dist/Chart.min.js").fail(() => { $.getScript(UrlCDN[1] + "ajax/libs/Chart.js/2.9.4/Chart.min.js"); }));
     });
-    $("meta:last").after($("<link>").attr({ "rel": "stylesheet", "href": "https://cdn.jsdelivr.net/npm/bootstrap@4/dist/css/bootstrap.min.css", "crossorigin": "anonymous" }).on("error", function () { this.href = "https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.2/css/bootstrap.min.css" }));
-
+    $("meta:last").after($("<link>").attr({ "rel": "stylesheet", "href": UrlCDN[0] + "npm/bootstrap@4/dist/css/bootstrap.min.css", "crossorigin": "anonymous" }).on("error", function () { this.href = UrlCDN[1] + "npm/bootstrap@4/dist/css/bootstrap.min.css" }));
     //$("body").css("background", "url('https://upload.wikimedia.org/wikipedia/sr/6/68/Saobra%C4%87ajni_fakultet_Doboj.png') right bottom / 200px no-repeat fixed padding-box padding-box");
     $("body").css("background", "url('https://sf.ues.rs.ba/cir/wp-content/uploads/2024/04/LogoPng.png') right bottom / 200px no-repeat fixed padding-box padding-box");
     $("div.container-fluid").addClass("p-3")
@@ -36,20 +39,20 @@ $(document).ready(() => {
     $("div.alert:eq(1)").addClass("alert-info").append($("<h4></h4>").addClass("alert-heading").text("Info"), $("<p></p>").addClass("mb-0").html("Podaci za ovu web aplikaciju su iskorišteni iz skripte <strong>Transport opasnih materija - ADR 2007</strong> čiji su autori Prof. dr Velibor Peulić, dr Radovan Višković, docent i Željko Matoc"));
     $("article").not(":first").prepend($("<hr>").addClass("my-2"));
     $("figure:eq(0)").addClass("figure text-center").append($("<img>").attr({ "src": "https://api.inpart24.com/uploads/yoofp01b.nze_adr-plate.jpg", "alt": "ADR Listica" }).addClass("figure-img img-fluid my-n3").css("max-height", "200px"), $("<figcaption></figcaption>").addClass("figure-caption text-center font-italic").text("Primjer naranžastog obilježja sa brojem za obilježavanje opasnosti (HIN) i UN brojem")).parent("div").addClass("d-flex justify-content-center");
-    $("div.card-footer").addClass("text-muted py-1").append($("<p></p>").addClass("mb-0 small text-center").html("Ovaj projekat koristi<img alt='Bootstrap Badge'>,<img alt='jQuery Badge'>,<img alt='Chart.js Badge'>preko<img alt='jsDelivr Badge'>CDN servera i hostovan je na<img alt='GitHub Pages Badge'>.<br>HTML kod ove stranice možete vidjeti na<a href='https://github.com/BaleshSrle" + location.pathname + "' hreflang='en' target='_blank'><img alt='Static Badge'></a>.<br>&copy; 2019 - " + new Date().getFullYear() + "."));
-    images[1].src = "https://img.shields.io/github/v/release/twbs/bootstrap?filter=v4.*&logo=bootstrap&logoColor=white&label=Bootstrap&color=7952b3";
-    images[1].onerror = function () { this.src = "https://badgen.net/jsdelivr/v/npm/bootstrap@4?color=7952b3&label=Bootstrap"; }
-    images[2].src = "https://img.shields.io/github/v/release/jquery/jquery?filter=3.*&logo=jquery&logoColor=white&label=jQuery&color=0769ad";
-    images[2].onerror = function () { this.src = "https://badgen.net/jsdelivr/v/npm/jquery@3?color=0769ad&label=jQuery"; }
-    images[3].src = "https://img.shields.io/github/v/release/chartjs/Chart.js?filter=v2.*&logo=chartdotjs&logoColor=white&label=Chart.js&color=ff6384";
-    images[3].onerror = function () { this.src = "https://badgen.net/jsdelivr/v/npm/chart.js@2?color=ff6384&label=Chart.js"; }
-    images[4].src = "https://img.shields.io/badge/jsDelivr-e84d3d?logo=jsdelivr&logoColor=white&labelColor=555555";
-    images[4].onerror = function () { this.src = "https://badgen.net/badge/icon/jsDelivr?&color=e84d3d&icon=jsdelivr&label"; }
-    images[5].src = "https://img.shields.io/badge/by%20GitHub-181717?logo=githubpages&logoColor=white&logoSize=auto&labelColor=222222";
-    images[6].src = "https://img.shields.io/badge/GitHub-181717?logo=github&logoSize=auto&labelColor=555555";
-    images[6].onerror = function () { "https://badgen.net/badge/icon/GitHub?color=181717&icon=github&label"; }
-    scripts[1].onerror = function () { this.src = "https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.2/js/bootstrap.min.js"; }
-    scripts[2].onerror = function () { this.src = "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js"; }
+    $("div.card-footer").addClass("text-muted py-1").append($("<p></p>").addClass("mb-0 small text-center").html("Ovaj projekat koristi<img alt='Bootstrap Badge'>,<img alt='jQuery Badge'>,<img alt='Chart.js Badge'>preko<img alt='jsDelivr Badge'>CDN servera i hostovan je na<img alt='GitHub Pages Badge'>.<br>HTML kod ove stranice možete vidjeti na<a href='" + GitHubUrl[1] + "BaleshSrle" + location.pathname + "' hreflang='en' target='_blank'><img alt='Static Badge'></a>.<br>&copy; 2019 - " + new Date().getFullYear() + "."));
+    images[1].src = BadgesUrl[0] + "github/v/release/twbs/bootstrap?filter=v4.*&logo=bootstrap&logoColor=white&label=Bootstrap&color=7952b3";
+    images[1].onerror = function () { this.src = BadgesUrl[1] + "jsdelivr/v/npm/bootstrap@4?color=7952b3&label=Bootstrap"; }
+    images[2].src = BadgesUrl[0] + "github/v/release/jquery/jquery?filter=3.*&logo=jquery&logoColor=white&label=jQuery&color=0769ad";
+    images[2].onerror = function () { this.src = BadgesUrl[1] + "jsdelivr/v/npm/jquery@3?color=0769ad&label=jQuery"; }
+    images[3].src = BadgesUrl[0] + "github/v/release/chartjs/Chart.js?filter=v2.*&logo=chartdotjs&logoColor=white&label=Chart.js&color=ff6384";
+    images[3].onerror = function () { this.src = BadgesUrl[1] + "jsdelivr/v/npm/chart.js@2?color=ff6384&label=Chart.js"; }
+    images[4].src = BadgesUrl[0] + "badge/jsDelivr-e84d3d?logo=jsdelivr&logoColor=white&labelColor=555555";
+    images[4].onerror = function () { this.src = BadgesUrl[1] + "badge/icon/jsDelivr?&color=e84d3d&icon=jsdelivr&label"; }
+    images[5].src = BadgesUrl[0] + "badge/by%20GitHub-181717?logo=githubpages&logoColor=white&logoSize=auto&labelColor=222222";
+    images[6].src = BadgesUrl[0] + "badge/GitHub-181717?logo=github&logoSize=auto&labelColor=555555";
+    images[6].onerror = function () { BadgesUrl[1] + "badge/icon/GitHub?color=181717&icon=github&label"; }
+    //scripts[1].onerror = function () { this.src = UrlCDN[1] + "ajax/libs/bootstrap/4.6.2/js/bootstrap.min.js"; }
+    //scripts[2].onerror = function () { this.src = UrlCDN[1] + "ajax/libs/Chart.js/2.9.4/Chart.min.js"; }
     $("img").slice(1).addClass("align-middle px-1");
     /* $("div.input-group").each(function () {
         $(this).children("div").addClass("input-group-prepend");
@@ -63,13 +66,10 @@ $(document).ready(() => {
         //$(this).slice(4, 7).attr("inputmode", "decimal");
         $(e).filter("#OutsideTemp, #ProduceWeigh, #ProduceTemp, #TransportDuration, #P1x, #P1y, #P2x, #P2y, #P3x, #P3y, #Sx, #Sy").attr("inputmode", "numeric");
         $(e).filter("#Q1, #Q2, #Q3").attr("inputmode", "decimal");
-        $(e).filter("#ProduceWeight, #TransportDuration").attr({ "min": "1", "step": "1" }).parent("div.form-group").tooltip({ placement: 'top', title: 'U ovo polje se unosi pozitivni cijeli broj.' });
-        $(e).filter("#OutsideTemp, #ProduceTemp").attr({ "min": "-50", "max": "50", "step": "1", "pattern": "-?[0-9]{2}" }).parent("div.form-group").tooltip({ placement: 'top', title: 'U ovo polje se unosi pozitivni ili negativni cijeli broj.' });
+        //$(e).filter("#ProduceWeight, #TransportDuration").attr({ "min": "1", "step": "1" }).parent("div.form-group").tooltip({ placement: 'top', title: 'U ovo polje se unosi pozitivni cijeli broj.' });
+        //$(e).filter("#OutsideTemp, #ProduceTemp").attr({ "min": "-50", "max": "50", "step": "1", "pattern": "-?[0-9]{2}" }).parent("div.form-group").tooltip({ placement: 'top', title: 'U ovo polje se unosi pozitivni ili negativni cijeli broj.' });
     });
-    $("table.table").each((_, e) => {
-        $(e).addClass("table-sm text-center").wrap($("<div></div>").addClass("table-responsive"));
-        $(e).children("caption").addClass("text-center").css("caption-side", "top");
-    });
+    $("table.table").addClass("table-sm text-center").wrap($("<div></div>").addClass("table-responsive")).children("caption").addClass("text-center").css("caption-side", "top");
     $("td").prev("th").addClass("align-middle");
     $("button.btn-primary").on("click", function () {
         var a = $("#RefrigerantType").val();
@@ -150,7 +150,7 @@ $(document).ready(() => {
         }
     }).attr("type", "button");
     $("select#ibo").on("click", function () {
-        const gistURL = 'https://gist.githubusercontent.com/BaleshSrle/4f23671359c5a30a1b05ac79c9b6783a/raw/adrMessages.json';
+        const gistURL = GitHubUrl[2] + 'BaleshSrle/4f23671359c5a30a1b05ac79c9b6783a/raw/adrMessages.json';
         $.getJSON(gistURL, function (adrMessage) {
             const adrHIN = $("#ibo").val();
             $("p.card-text").last().addClass("my-3").html(adrMessage[adrHIN] ?? "");
